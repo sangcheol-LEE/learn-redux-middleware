c93b9cd7309ba7f00f58b48a1aca84b7f2ed52bf
이상철 <zlrz6706@gmail.com>
2022-08-18 20:56:26 +0900

    초기상태 - 복습

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Sample from "./components/Sample"
 
 const App = () => {
   return (
     <div>
-      <Sample />
+      
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Provider } from 'react-redux';
-import { applyMiddleware, createStore } from 'redux';
-import rootReducer from './modules';
-import loggerMiddleware from './lib/loggerMiddleware';
-import ReduxThunk from "redux-thunk"
 
-const store = createStore(rootReducer, applyMiddleware(loggerMiddleware, ReduxThunk));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={store}>
         <App />
-    </Provider>
 );
 
 
